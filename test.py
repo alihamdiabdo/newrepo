a51fc972c41d32873bf1406b668b4f326ca696b9
iadel93 <engsmsm93@gmail.com>
2020-12-01 12:12:05 +0200

    this is my branch

--- test.py
+++ test.py
@@ -1,41 +1,3 @@
-# # # # # l = []
-# # # # # l.append(input("Enter Name and number"))
-# # # # # if l == q:
-# # # # #     print(l) 
-# # # # #     else:
-# # # # #         l.append(input("Enter Name and number"))
-
-# # # # # d = {k}
-
-# # # # x = input("please input:")
-# # # # l=[]
-# # # # while x != 'q' :
-# # # #     l.append(x)
-# # # #     x = input("please input:")
-     
-# # # # for i in range(len(l)):
-# # # #     print(l[i])
-
-# # # l = []
-# # # s = input("Enter pair of data:")
-# # # while(s!='q'):
-# # #     l.append(s)
-# # #     s = input("Enter pair of data:")
-# # # l = sorted(l)
-# # # print(l)
-
-# # # d={}
-# # # for i in l:
-# # #     splited = i.split(':')
-# # #     d.setdefault(splited[0],splited[1])
-# # # print(d)
-
-      
-# # x = "hello world"
-# # print(x[2:2])     
-
-# x = [10,20,30,40,50]
-# print(x[:3])
 
 x = 2
 chr(x)
@@ -48,3 +10,7 @@ x = "hello world"
 print(x[2:2]) 
  for i in range(len(l)):
      print(l[i])
+	 
+	 
+for i in range(50):
+	print(i)
